ca2875094874f425e1862350701cded20de53cac
Rich <richbport@yahoo.com>
2026-01-12 15:07:01 -0500

    string concatenation

--- index.js
+++ index.js
@@ -2,3 +2,6 @@
 const welcomeMessage = "Hello"; // string (const)
 let userName = "John"; // string (let can change)
 let userAge = 20; // number
+
+// Output greeting
+console.log(welcomeMessage + ", " + userName + "!"); // string concantenation
