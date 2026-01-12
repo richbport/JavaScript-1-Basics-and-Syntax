807a9e928626cda0a16600cfec202524a30248e8
Rich <richbport@yahoo.com>
2026-01-12 15:33:25 -0500

    typeof

--- index.js
+++ index.js
@@ -1,18 +1,5 @@
-// Variables
-const welcomeMessage = "Hello"; // string (const)
-let userName = "John"; // string (let can change)
-let userAge = 20; // number
+const a = 10;
+const b = 2.5;
 
-// Output greeting
-console.log(welcomeMessage + ", " + userName + "!"); // string concantenation
-
-// Using operators
-let nextYearAge = userAge + 1; // arithmetic operator
-
-// Display age information
-console.log("You are " + userAge + " years old.");
-console.log("Next year, you will be " + nextYearAge + ".");
-
-// Data type examples
-let isAdult = userAge >= 18; // boolean using comparison operator
-console.log("Are you an adult? " + isAdult);
+console.log(typeof a, a);
+console.log(typeof b, b);
