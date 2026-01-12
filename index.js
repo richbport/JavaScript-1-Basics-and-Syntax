46f45d0c27abbdc6fac172c6a92264e23e5447dd
Rich <richbport@yahoo.com>
2026-01-12 14:23:02 -0500

    variables

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-let x = 10;
-let y = 2.5;
-console.log(x + y); 
-
-console.log(0 / 0); // NaN
-console.log(5 / 0); // Infinity
+// Variables
+const welcomeMessage = "Hello"; // string (const)
+let userName = "John"; // string (let can change)
+let userAge = 20; // number
